2a2cc3a513e80ed8dcd163f33fe256e62089df06
Karsei <karseidev@gmail.com>
2023-01-01 23:48:02 +0900

    feat: added basic config

---
+++ src/main/java/kr/pe/karsei/springsecuritydemo/SecurityController.java
@@ -0,0 +1,12 @@
+package kr.pe.karsei.springsecuritydemo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SecurityController {
+    @GetMapping("/")
+    public String index() {
+        return "home";
+    }
+}
