df81f80af5481627c71a03e73a80cf66f676b1ae
Karsei <karseidev@gmail.com>
2023-01-05 20:58:23 +0900

    feat: added login, logout

--- src/main/java/kr/pe/karsei/springsecuritydemo/SecurityConfig.java
+++ src/main/java/kr/pe/karsei/springsecuritydemo/SecurityConfig.java
@@ -1,22 +1,30 @@
 package kr.pe.karsei.springsecuritydemo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private UserDetailsService userDetailsService;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
@@ -46,6 +54,37 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                     }
                 }) // 로그인 실패 후 핸들러
                 .permitAll()
+        ;
+        http
+                // 로그아웃 처리
+                .logout()
+                // 로그아웃 처리 URL
+                .logoutUrl("/logout")
+                // 로그아웃 성공 후 이동 페이지
+                .logoutSuccessUrl("/login")
+                // 로그아웃 후 쿠키 삭제
+                .deleteCookies("JSESSIONID", "remember-me")
+                .addLogoutHandler(new LogoutHandler() {
+                    @Override
+                    public void logout(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
+                        HttpSession session = request.getSession();
+                        session.invalidate();
+                    }
+                })
+                .logoutSuccessHandler(new LogoutSuccessHandler() {
+                    @Override
+                    public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+                        System.out.println("logout");
+                        response.sendRedirect("/login");
+                    }
+                })
+        ;
+        http
+                .rememberMe() // remember
+                .rememberMeParameter("remember") // 기본파라미터명은 remember-me
+                .tokenValiditySeconds(3600) // 유지시간으로서 1시간. 기본값은 14일
+                .alwaysRemember(true) // remember-me 기능이 활성화되지 않아도 항상 실행
+                .userDetailsService(userDetailsService) // 시스템에 있는 사용자를 처리하는 과정에 필요한 것
         ;
     }
 }
