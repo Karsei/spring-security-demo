3274ef262d9f91f43f29dce3bcab021aec47f9e9
Karsei <karseidev@gmail.com>
2023-01-21 20:19:00 +0900

    refact: changed project to multimodule

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Spring Security Demo
 
-내부 핵심 동작 원리 파악용
+Inflearn `정수원` 님의 `스프링 시큐리티 - Spring Boot 기반으로 개발하는 Spring Security` 을 들으며 공부한 것
+
+* architecture - 기본 API, Filter, 주요 아키텍쳐 이해(섹션 1, 2)
---
+++ form/src/main/java/kr/pe/karsei/springsecuritydemo/SpringSecurityDemoApplication.java
@@ -0,0 +1,13 @@
+package kr.pe.karsei.springsecuritydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@EnableWebSecurity
+@SpringBootApplication
+public class SpringSecurityDemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityDemoApplication.class, args);
+    }
+}
