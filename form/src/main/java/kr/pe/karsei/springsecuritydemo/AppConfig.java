ac056019e98bd10624786823840c903e205f7a47
Karsei <karseidev@gmail.com>
2023-01-23 12:37:25 +0900

    feat: ready view templates

---
+++ form/src/main/java/kr/pe/karsei/springsecuritydemo/AppConfig.java
@@ -0,0 +1,7 @@
+package kr.pe.karsei.springsecuritydemo;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+}
---
+++ form/src/main/java/kr/pe/karsei/springsecuritydemo/HomeController.java
@@ -0,0 +1,17 @@
+package kr.pe.karsei.springsecuritydemo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+	@GetMapping
+	public String home() {
+		return "home";
+	}
+
+	@GetMapping(value = "login")
+	public String login()  {
+		return "login";
+	}
+}
