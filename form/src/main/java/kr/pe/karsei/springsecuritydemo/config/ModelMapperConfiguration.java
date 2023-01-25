d7c6b9a8cb07e8364974267e09118b0a0818fc47
Karsei <karseidev@gmail.com>
2023-01-25 15:30:04 +0900

    docs: added a comment

--- form/src/main/java/kr/pe/karsei/springsecuritydemo/config/ModelMapperConfiguration.java
+++ form/src/main/java/kr/pe/karsei/springsecuritydemo/config/ModelMapperConfiguration.java
@@ -10,6 +10,7 @@ public class ModelMapperConfiguration {
     public ModelMapper modelMapper() {
         ModelMapper mapper = new ModelMapper();
         mapper.getConfiguration()
+                // Setter 를 하지 않고도 private 로 선언된 멤버가 있으면 매핑하도록 함
                 .setFieldAccessLevel(org.modelmapper.config.Configuration.AccessLevel.PRIVATE)
                 .setFieldMatchingEnabled(true);
         return mapper;
